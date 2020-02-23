65abc9774d1f6f727c8e5a65ebdeb9156f987e95
sisisin <azsisisin@gmail.com>
2020-02-23 15:32:30 +0900

    update

--- src/app/testHelpers/storybook.tsx
+++ src/app/testHelpers/storybook.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { DefaultTypelessProvider } from 'typeless';
 
 export const AppProvider: React.FC = (props) => {
-  return <DefaultTypelessProvider>{props.children as any}</DefaultTypelessProvider>;
+  return <DefaultTypelessProvider>{props.children}</DefaultTypelessProvider>;
 };
