13451f3d12061957e2e53e25da9000cd9dfcb39f
sisisin <azsisisin@gmail.com>
2020-02-23 12:10:32 +0900

    feat: add module generator

--- plopfile.js
+++ plopfile.js
@@ -29,4 +29,22 @@ module.exports = function generate(plop) {
       },
     ],
   });
+  plop.setGenerator('module', {
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'choose module name in camelCase (e.g. myModule)',
+        basePath: '.',
+      },
+    ],
+    actions: [
+      {
+        type: 'addMany',
+        destination: path.join(__dirname, 'src/app/features'),
+        base: '.blueprints/module',
+        templateFiles: '.blueprints/module/**/**',
+      },
+    ],
+  });
 };
