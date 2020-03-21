2d16078d55063dd5e4bea7f810c4c188eefd4e22
sisisin <azsisisin@gmail.com>
2020-03-21 18:33:30 +0900

    feat: keep session

--- src/app/features/session/interface.ts
+++ src/app/features/session/interface.ts
@@ -4,7 +4,7 @@ import { User } from 'app/types/User';
 
 // --- Actions ---
 export const [handle, SessionActions, getSessionState] = createModule(SessionSymbol)
-  .withActions({ loginSucceeded: (user: User) => ({ payload: { user } }) })
+  .withActions({ $mounted: null, loginSucceeded: (user: User) => ({ payload: { user } }) })
   .withState<SessionState>();
 
 // --- Types ---
--- src/app/features/session/module.ts
+++ src/app/features/session/module.ts
@@ -1,8 +1,11 @@
 import { appHistory } from 'app/services/appHistory';
 import { handle, SessionActions, SessionState } from './interface';
 
+const sessKey = 'sess_key';
+
 // --- Epic ---
-export const epic = handle.epic().on(SessionActions.loginSucceeded, () => {
+export const epic = handle.epic().on(SessionActions.loginSucceeded, ({ user }) => {
+  localStorage.setItem(sessKey, JSON.stringify(user));
   const from = appHistory.location.searchParams.get('from');
   const path = from ? decodeURIComponent(from) : '/';
   appHistory.push({ path });
@@ -14,9 +17,17 @@ const initialState: SessionState = {
   user: undefined,
 };
 
-export const reducer = handle.reducer(initialState).on(SessionActions.loginSucceeded, (state, { user }) => {
-  state.user = user;
-});
+export const reducer = handle
+  .reducer(initialState)
+  .on(SessionActions.$mounted, (state) => {
+    const item = localStorage.getItem(sessKey);
+    if (item) {
+      state.user = JSON.parse(item);
+    }
+  })
+  .on(SessionActions.loginSucceeded, (state, { user }) => {
+    state.user = user;
+  });
 
 // --- Module ---
 export const useSessionModule = handle;
