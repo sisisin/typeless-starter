17fdeabac2ca3c8f8ab95923235078b98bd9bb06
sisisin <azsisisin@gmail.com>
2020-04-15 01:15:51 +0900

    chore: gen test

--- plopfile.js
+++ plopfile.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { execSync } = require('child_process');
 module.exports = function generate(plop) {
   plop.setActionType('addRoute', async (answers, config, plop) => {
-    const route = plop.renderString('{{snakeCase name}}', answers);
+    const route = plop.renderString('/{{snakeCase name}}', answers);
     const moduleName = plop.renderString('{{pascalCase name}}Module', answers);
     const options = `-r ${route} -p ${answers.name} -t ${answers.name} -m ${moduleName}`;
 
@@ -18,17 +18,37 @@ module.exports = function generate(plop) {
         basePath: '.',
       },
     ],
-    actions: [
-      {
-        type: 'addMany',
-        destination: path.join(__dirname, 'src/app/features'),
-        base: '.blueprints/feature',
-        templateFiles: '.blueprints/feature/**/**',
-      },
-      {
-        type: 'addRoute',
-      },
-    ],
+    actions: () => {
+      const appTestPath = path.join(__dirname, 'src/app/App.test.tsx');
+      return [
+        {
+          type: 'addMany',
+          destination: path.join(__dirname, 'src/app/features'),
+          base: '.blueprints/feature',
+          templateFiles: '.blueprints/feature/**/**',
+        },
+        { type: 'addRoute' },
+        {
+          type: 'modify',
+          path: appTestPath,
+          pattern: /^/,
+          template: `import { {{pascalCase name}}Module } from 'app/features/{{name}}/module';
+`,
+        },
+        {
+          type: 'modify',
+          path: appTestPath,
+          pattern: /$/g,
+          template: `
+it('renders {{name}} with /', async () => {
+  const node = render(<App></App>, { withAuth: true });
+  await navigateAndWaitForRendered('/{{snakeCase name}}', {});
+  expect(node).toRenderComponent({{pascalCase name}}Module);
+});
+`,
+        },
+      ];
+    },
   });
   plop.setGenerator('module', {
     prompts: [
