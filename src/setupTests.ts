50e2afcdb72e05c127ad7d9b1f9358c4a41c96bf
sisisin <azsisisin@gmail.com>
2020-04-14 21:03:04 +0900

    fix: custom test assertion

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,14 +2,14 @@ import { ReactTestRenderer } from 'react-test-renderer';
 
 expect.extend({
   toRenderComponent(received: ReactTestRenderer, component: () => JSX.Element) {
-    const pass = received.root.findByType(component)?.type === component;
+    const pass = received.root.findAllByType(component)[0]?.type === component;
     if (pass) {
       return {
-        message: () => `expected node not to render component ${component}`,
+        message: () => `expected node not to render component \`${component.name}\``,
         pass,
       };
     } else {
-      return { message: () => `expected node to render component ${component}`, pass: !pass };
+      return { message: () => `expected node to render component \`${component.name}\``, pass };
     }
   },
 });
